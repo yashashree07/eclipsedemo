4b98a92b5903379f539beb0b26baeb888387a628
yashashree07 <suryawanshiyashashree@gmail.com>
2020-12-16 10:10:54 +0530

    first commit

---
+++ firstJavaProject/src/firstJavaProject/FirstJavaclass.java
@@ -0,0 +1,14 @@
+package firstJavaProject;
+
+public class FirstJavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=1,n2=2;
+		int add=n1+n2;
+		
+		System.out.println("Result : "+add);
+
+	}
+
+}
